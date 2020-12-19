e8bcaf78dbb2b95b73ee993b61dc85e3fc2710bc
SURYAVANSHI SNEHAL BALKRISHNA <58397197+snehal2841@users.noreply.github.com>
2020-12-19 11:45:53 +0530

    middleware added

--- app.js
+++ app.js
@@ -52,6 +52,13 @@ passport.use(new LocalStrategy({
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+/*app.use((req,res,next)=>{
+    res.locals.currentUser=req.user;
+    res.locals.success=req.flash("success");
+    res.locals.error=req.flash("error");
+    next();
+});*/
+
 var userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
